3e60cbe89b4aa1118c8c1933f7ce4489de1d935c
Fabri31295 <fabricioalmaraz123@gmail.com>
2022-05-05 15:15:40 -0300

    Correccion en rutas

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 require __DIR__.'/auth.php';
 
 Route::group(['middleware' => 'auth'], function() {
